f5a47275facd4fd91eba009d042d97035c3ea01a
kaszamas <kaioandrade78@gmail.com>
2023-09-21 21:30:10 -0300

    atmks

---
+++ lib/views/cliente.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Cliente extends StatefulWidget {
+  const Cliente({super.key});
+
+  @override
+  State<Cliente> createState() => _ClienteState();
+}
+
+class _ClienteState extends State<Cliente> {
+  @override
+  Widget build(BuildContext context) {
+   return  Scaffold(
+     backgroundColor: Colors.white,
+      appBar: AppBar(title: Text("Cliente"), backgroundColor: Color.fromARGB(255, 23, 153, 90),),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        
+      child: Column(
+        
+        
+        children: [
+          const SizedBox(
+              height: 34,
+            ),
+          
+        Row(
+          
+          children: [
+            Image.asset("imagem/detalhe_cliente.png"),
+            const Text("Nosso cliente",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.green
+                
+                
+            ),
+            
+            
+            )
+            
+
+          ],
+        ),
+        const SizedBox(
+              height: 34,
+            ),
+        const Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+         textAlign: TextAlign.justify,)
+      ],)
+      ),
+      
+    );
+    
+    
+  }
+  
+}
